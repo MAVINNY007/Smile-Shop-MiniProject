c2f0d8051f078f65fbbbe706876eb46c5bf4aa9d
MAVINNY007 <98442468+MAVINNY007@users.noreply.github.com>
2022-05-15 20:17:03 +0700

    Create credits.php

---
+++ credits.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smile_Shop</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body><br>
+    <section style="background-color: #000000;">
+        <img src="images/smile-s-logo-1.png" alt="image">
+    <div class="popo">
+        <a style="color:#fff;" href="index.php"> - กลับหน้าหลัก - </a>
+    </div><br>
+    </section>
+
+        <!-- Contents -->
+        <section class="input-group">
+        <p>********************************************************************************</p>
+            <div>
+                <h1 class="header">กลุ่มผู้จัดทำ</h1><br>
+                <div class="row">
+                    <div class="column">
+                                <img src="images/no.jpg" alt="image" style="height: 275px; width: 75%; text-align: center;border: 5px solid rgb(0, 0, 0);border-radius: 25px;">
+                                <h2>นส. กนกพร คุปตธรณี</h2>
+                                <h4>1630900213</h4>
+                    </div>
+                    <div class="column">
+                                <img src="images/ma.jpg" alt="image" style="width: 75%; text-align: center;border: 5px solid rgb(0, 0, 0);border-radius: 25px;">
+                                <h2>นาย มาวิน วามะลุน</h2>
+                                <h4>1630900205</h4>
+                    </div>
+                    <div class="column">
+                                <img src="images/sei.jpg" alt="image" style="height: 275px; width: 75%; text-align: center;border: 5px solid rgb(0, 0, 0);border-radius: 25px;">
+                                <h2>นาย กูซัยฟีย์ ปาเซเลาะ</h2>
+                                <h4>1630900429</h4>
+                    </div>
+                </div> 
+            </div>
+        <p>********************************************************************************</p>
+           
+        <div>
+            <h1 class="header">นำเสนออาจารย์</h1><br>
+                <img src="images/ter.jpg" alt="image" style="width: 30%; text-align: center;border: 5px solid rgb(0, 0, 0);border-radius: 25px;">
+                <h2>อาจารย์ทศพล บ้านคลองสี่</h2>
+                <h4>วิชาการเขียนโปรแกรมคอมพิวเตอร์ 2 คณะวิศวกรรมศาสตร์ มหาวิทยาลัยกรุงเทพ ภาคเรียนที่ 2 ปีการศึกษา 2564</h4>
+                <h4>โดย โครงการนี้เป็นส่วนหนึ่งของวิชา : CE223 section : 236A</h4>
+            <p></p>
+            <p>********************************************************************************</p>
+        </div>
+        </section>
+</body>
+</html>
